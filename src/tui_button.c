96b70a471dbf3bfa99575c3f845070d68c97454d
Bruno Hudley <191642467+Brunohudley@users.noreply.github.com>
2026-08-09 15:18:48 -0300

    Add copyright notice to tui_button.c

--- src/tui_button.c
+++ src/tui_button.c
@@ -1,3 +1,13 @@
+// ============================================================================
+// Copyright (c) 2026 Brunohudley. All rights reserved.
+//
+// This code is free software: you can redistribute it and/or modify it 
+// under the terms of the MIT License. The above copyright notice must be 
+// included in all copies or substantial portions of the software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+// ============================================================================
+
 #include "../include/tui_button.h"
 #include "../include/tui_widgets.h"
 
